c360161d1592e2228d5f701fc80978ae66c9c02b
Henrique Marques <henriquedms7@gmail.com>
2023-08-25 11:49:08 +0200

    Handle the submit event

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,21 @@
 export default function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    // console.log(data);
+    // onAddActivity(data);
+    event.target.reset();
+    // console.log(event.target.elements);
+    event.target.elements[0].focus();
+  }
   return (
-    <form>
-      <h1>Test</h1>
+    <form onSubmit={handleSubmit}>
+      <h1>Add new Activity:</h1>
       <label htmlForm="activity-name">Name:</label>
-      <input type="text" id="activity-name" />
+      <input type="text" id="activity-name" name="name" />
       <label htmlForm="weather-status">Good-weather activity:</label>
-      <input type="checkbox" id="weather-status" />
+      <input type="checkbox" id="weather-status" name="isForGoodWeather" />
       <button>Submit</button>
     </form>
   );
