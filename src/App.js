e91521b77259eb8365d7bcf5b763d80df141c16c
Stephan Bergmann <bergmann.stephan.1@gmx.de>
2023-08-25 12:28:01 +0200

    add handleAddActivity

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import Form from "./components/Form/Form.js";
+import { useState } from "react";
+import { uid } from "uid";
 
 export default function App() {
+  const [activities, setActivities] = useState([]);
+
+  function handleAddActivity(activity) {
+    setActivities([...activities, { id: uid(), ...activity }]);
+  }
+
   return (
     <main>
-      <Form />
+      <Form onAddActivity={handleAddActivity} />
     </main>
   );
 }
